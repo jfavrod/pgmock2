d2b6ce0f263fdcfeee2ec7f7003d525e1a3b10d4
Jason Favrod <jason.favrod@monsanto.com>
2019-09-25 16:27:22 -1000

    Starting Translation to TypeScript

---
+++ src/interfaces.ts
@@ -0,0 +1,7 @@
+import { QueryResult } from 'pg';
+
+export interface IPGClient {
+    end: () => Promise<void>;
+    query(sql: string, values: any[]): Promise<QueryResult>;
+    release: () => Promise<void>;
+}
