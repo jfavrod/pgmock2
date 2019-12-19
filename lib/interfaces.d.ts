8f24736f477301344fca73d13178b9dd47063667
Jason Favrod <jason.favrod@monsanto.com>
2019-12-19 07:44:05 -1000

    Removed lib From Repo

--- lib/interfaces.d.ts
+++
@@ -1,6 +0,0 @@
-import { QueryResult } from 'pg';
-export interface IPGClient {
-    end: () => Promise<void>;
-    query(sql: string, values: any[]): Promise<QueryResult>;
-    release: () => Promise<void>;
-}
