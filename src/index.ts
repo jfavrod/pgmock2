5520e91b6023df4354e3425c1c8bfe34288b593e
Jason Favrod <jason.favrod@monsanto.com>
2020-01-09 15:24:15 -1000

    Cleaned Up Build; Added getPool and getClient Methods

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Client, Pool } from 'pg';
+import PGMock2 from './PGMock2';
+
+export default PGMock2;
+
+export function getClient() {
+    const pgmock = new PGMock2();
+    return pgmock as unknown as Client;
+}
+
+export function getPool(pgmock?: PGMock2) {
+    pgmock = pgmock || new PGMock2();
+    return pgmock as unknown as Pool;
+}
